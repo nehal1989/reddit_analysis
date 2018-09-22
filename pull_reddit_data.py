acbe2cc38a3754140bd71a0c78bf1d8e31b5ce70
Nehal <nehal26@gmail.com>
2018-09-22 21:39:54 +0100

    Initial set up and commit.

--- pull_reddit_data.py
+++ pull_reddit_data.py
@@ -1,13 +1,19 @@
-import pandas
 import praw
-import datetime as dt
 
 with open('reddit.config') as file:
     login_details = file.read().splitlines()
 
-reddit = praw.Reddit(client_id=login_details[0], \
-                     client_secret=login_details[1], \
-                     user_agent=login_details[2], \
-                     username=login_details[3], \
-                     password=login_details[4])
+reddit = praw.Reddit(client_id=login_details[0],
+                     client_secret=login_details[1],
+                     password=login_details[2],
+                     user_agent=login_details[3],
+                     username=login_details[4])
 
+print(reddit.user.me())
+
+climbing_sr = reddit.subreddit('Climbing')
+
+top_climbing = climbing_sr.top(limit=100)
+
+for thing in top_climbing:
+    print(thing.title)
